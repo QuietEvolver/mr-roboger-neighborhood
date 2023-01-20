7b819dffa37724ce97c6bbaddbca896aa33a19f5
Vera Weikel <vera.weikel@gmail.com>
2023-01-20 12:10:21 -0800

    Update JS to return Won't you be my neighbor? for numbers that contain a 3 in a given index.

--- js/scripts.js
+++ js/scripts.js
@@ -66,6 +66,26 @@ Return a list of values from 0 to the user's inputter number.
 // }
 
 // test6:
+// function neighborhoodMessage(num){
+//   const indexedArray = [];// Getting the string as a parameter
+//   // and typecasting it into an integer
+  
+//   for (let i = 0; i <= num; i++) {
+//     let myFunc = num => Number(num);
+//     const intArr = Array.from(String(i), myFunc);  // eachIndexOfArray
+//     // Print the result array
+//     console.log(intArr);
+//     if(intArr.includes(1)){
+//       indexedArray.push("Beep!");
+//     } else if(intArr.includes(2)){
+//       indexedArray.push("Boop!");
+//     } else indexedArray.push(i);
+//   }
+//   console.log("IndexArr: ", indexedArray);
+//   return indexedArray; 
+// }
+
+// test7: 
 function neighborhoodMessage(num){
   const indexedArray = [];// Getting the string as a parameter
   // and typecasting it into an integer
@@ -79,6 +99,8 @@ function neighborhoodMessage(num){
       indexedArray.push("Beep!");
     } else if(intArr.includes(2)){
       indexedArray.push("Boop!");
+    } else if(intArr.includes(3)){
+      indexedArray.push("Won't you be my neighbor?");
     } else indexedArray.push(i);
   }
   console.log("IndexArr: ", indexedArray);
