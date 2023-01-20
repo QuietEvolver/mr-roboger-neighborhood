b39e081649789ba1226a7ffe18b3a98c5b18d96a
Vera Weikel <vera.weikel@gmail.com>
2023-01-20 15:07:48 -0800

    It will remove comments and console logs.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 
 [UI/UX]:Design the application so that a user is able to enter a new number and see new results over and over again.
 Return a list of values from 0 to the user's inputter number. 
-[HTML]:Printing each element in the list to the webpage. 
 
 */
 
@@ -15,18 +14,16 @@ function neighborhoodMessage(num){
   // and typecasting it into an integer
   for (let i = 0; i <= num; i++) {
     let myFunc = num => Number(num); 
-    const intArr = Array.from(String(i), myFunc);  // eachIndexOfArray
+    const intArr = Array.from(String(i), myFunc);
     // Print the result array
-    // console.log(intArr);
     if(intArr.includes(3)){
-    indexedArray.push("Won't you be my neighbor?");
-  } else if(intArr.includes(2)){
-    indexedArray.push("Boop!");
-  } else if(intArr.includes(1)){
-    indexedArray.push("Beep!");
-  }  else indexedArray.push(i);
+      indexedArray.push("Won't you be my neighbor?");
+    } else if(intArr.includes(2)){
+      indexedArray.push("Boop!");
+    } else if(intArr.includes(1)){
+      indexedArray.push("Beep!");
+    }  else indexedArray.push(i);
   }
-  console.log("IndexArr: ", indexedArray);
   return indexedArray; 
 }
 
