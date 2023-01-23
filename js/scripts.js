04f01618b91f6bf5e0e518cfc3327e82987071fb
Vera Weikel <vera.weikel@gmail.com>
2023-01-22 19:44:40 -0800

    It will update scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function neighborhoodMessage(num, messageA){
+function neighborhoodMessage(num, messageA, messageB, messageC){
   const indexedArray = [];
   if(num==="" || num===undefined || num===null || num===Symbol){ 
     return 0;
@@ -10,11 +10,11 @@ function neighborhoodMessage(num, messageA){
     const intArr = Array.from(String(i), myFunc); 
     // Print the result array
     if(intArr.includes(3)){
-    indexedArray.push(messageA, messageB);
+    indexedArray.push(messageA);
   } else if(intArr.includes(2)){
     indexedArray.push(messageB);
   } else if(intArr.includes(1)){
-    indexedArray.push("Beep!");
+    indexedArray.push(messageC);
   }  else indexedArray.push(i);
   }
   console.log("IndexArr: ", indexedArray);
@@ -27,8 +27,9 @@ function handleFormSubmit(e){
   const input = document.querySelector("input#input-id").value;
   const messageA = "Won't you be my neighbor?";
   const messageB = "Boop!";
-  neighborhoodMessage(input, messageA, messageB);
-  const output = neighborhoodMessage(input, messageA, messageB);
+  const messageC = "Beep!";
+  neighborhoodMessage(input, messageA, messageB, messageC);
+  const output = neighborhoodMessage(input, messageA, messageB, messageC);
 
   document.querySelector("span#output").innerText = output;
 
